2cd55c271dfbb30d0bebcea346bd49262580b64b
Documenter.jl <documenter@juliadocs.github.io>
2025-06-25 13:42:32 +0000

    build based on 2cede1d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GenDav","page":"Home","title":"GenDav","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GenDav.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}

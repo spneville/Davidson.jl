d48ce7644a162f8f317cae5b96861e31e9da02db
spneville <sneville.sneville@gmail.com>
2025-06-27 17:49:10 -0400

    First pass completed

--- src/solver.jl
+++ src/solver.jl
@@ -60,7 +60,7 @@ function guessvec(cache::Cache)
 end
 
 function run_gendav(cache::Cache, verbose::Bool)
-    
+
     #
     # Initialisation
     #
@@ -68,7 +68,6 @@ function run_gendav(cache::Cache, verbose::Bool)
     # nnew:    the no. new subspace vectors added in a given iteration
     # nconv:   the no. of converged roots
     # nsigma:  the total no. sigma vectors calculated
-
     cache.currdim = cache.blocksize
     cache.nnew = cache.blocksize
     cache.nconv = 0
@@ -97,9 +96,24 @@ function run_gendav(cache::Cache, verbose::Bool)
         if verbose print_report(k, cache) end
 
         # Stop here if all the roots are converged
-        
-        
-        exit()
+        if all(i -> i == 1, cache.iconv[1:cache.nroots]) break end
+
+        # Compute the correction vectors
+        correction_vectors(cache)
+
+        # Compute the new subspace vectors
+        subspace_vectors(cache)
+
+        # Subspace collapse?
+        if cache.currdim + cache.nnew * 2 > cache.maxvec
+            subspace_collapse(cache)
+        else
+            cache.currdim = cache.currdim + cache.nnew
+        end
+
+        # If we are here and this is the last iteration, then
+        # we failed to converge all roots
+        if k == cache.niter @warn "Not all roots converged" end
         
     end
     
@@ -198,7 +212,7 @@ function residual_vectors(cache::Cache)
     # Dimensions
     @unpack matdim, currdim, blocksize, nnew, tol = cache
     
-    # Subspace eigenvectors
+    # Subspace eigenvectors    
     alpha = reshape(view(cache.alpha, 1:currdim^2), (currdim, currdim))
 
     # Subspace eigenvalues
@@ -286,4 +300,164 @@ function print_report(k::Int64, cache::Cache)
 
     println("$k $currdim $maxres $nconv")
     
+end
+
+function correction_vectors(cache::Cache)
+
+    @unpack matdim, currdim, nnew, hdiag, rho1 = cache
+
+    bvec = cache.bvec
+    
+    #
+    # Diagonal preconditioned residue correction vectors
+    #
+
+    # Indices of the positions in the bvec array in which the
+    # correction vectors will be stored
+    ki = currdim + 1
+    kf = currdim + nnew
+    
+    # Loop over correction vectors
+    k1 = 0
+    for k in ki:kf
+
+        k1 += 1
+        
+        # Loop over elements of the correction vector
+        for i in 1:matdim
+            bvec[i,k] = -bvec[i,k] / (hdiag[i]-rho1[k1])
+        end
+            
+    end
+
+end
+
+function subspace_vectors(cache::Cache)
+
+    @unpack T, currdim, nnew = cache
+
+    bvec = cache.bvec
+    sigvec = cache.sigvec
+    work2 = cache.work2
+    
+    # Indices of the positions in the bvec array in which the new
+    # subspace vectors will be stored
+    ki = currdim + 1
+    kf = currdim + nnew
+    
+    #
+    # New orthonormalisation of the correction vectors
+    #
+    # Performed in two steps:
+    #
+    # (1) Gram-Schmidt orthogonalisation against the previous subspace
+    #     vectors
+    #
+    # (2) Symmetric orthogonalisation within the space spanned by the
+    #     intermediately orthogonalised correction vectors from (1)
+    #
+
+    # Overlaps between the previous subspace vectors and the correction
+    # vectors
+    Smat = Matrix{T}(undef, currdim, nnew)
+    bprev = view(bvec, :, 1:currdim)
+    bnew = view(bvec, :, ki:kf)
+    BLAS.gemm!('T', 'N', 1.0, bprev, bnew, 0.0, Smat)
+
+    # GS orthogonalisation of the correction vectors against the previous
+    # subspace vectors
+    k1 = 0
+    for k in ki:kf
+        k1 += 1
+        for i in 1:currdim
+            bvec[:,k] = bvec[:,k] - Smat[i,k1] * bvec[:,i]
+        end
+        bvec[:,k] = bvec[:,k] / sqrt(dot(bvec[:,k], bvec[:,k]))
+    end
+
+    # Overlaps between the intermediately orthogonalised correction
+    # vectors
+    Smat = Matrix{T}(undef, nnew, nnew)
+    BLAS.gemm!('T', 'N', 1.0, bnew, bnew, 0.0, Smat)
+
+    # Inverse square root of the overlap matrix
+    Sinvsq = invsqrt_matrix(Smat, nnew)
+
+    # Symmetric orthogonalisation of the intermediately orthogonalised
+    # correction vectors amongst themselves
+    w2 = view(work2, :, 1:nnew)
+    BLAS.gemm!('N', 'N', 1.0, bnew, Sinvsq, 0.0, w2)    
+    copy!(bnew, w2)
+
+end
+
+function invsqrt_matrix(mat::Matrix{Float64}, dim::Int64)
+
+    # Diagonalisation of the input matrix
+    F = eigen(mat)
+    eigvec = F.vectors
+    lambda = F.values
+
+    # Inverse square root of the input matrix
+    dmat = Matrix{Float64}(undef, dim, dim)
+    fill!(dmat, 0.0)
+
+    thrsh = 1e-10
+    
+    for i in 1:dim
+
+       if lambda[i] > thrsh && lambda[i] < 0.0
+           @error "Non semi positive definite matrix encountered"
+       end
+
+        if lambda[i] > thrsh
+            dmat[i,i] = 1.0 / sqrt(abs(lambda[i]))
+        end
+            
+    end
+
+    invsqrtmat = eigvec * dmat * transpose(eigvec)
+
+    return invsqrtmat
+    
+end
+
+function subspace_collapse(cache::Cache)
+
+    #
+    # Compute the Ritz vectors. We will use the sigvec array as a
+    # working array here to store the Ritz vectors
+    #
+    
+    # Initialisation
+    blocksize = cache.blocksize
+    currdim = cache.currdim
+    sigvec = cache.sigvec
+    fill!(sigvec, 0.0)
+    alpha = reshape(view(cache.alpha, 1:currdim^2), (currdim, currdim))
+    bvec = cache.bvec
+    
+    # Loop over Ritz vectors
+    for k in 1:blocksize
+
+       # Compute the kth Ritz vector 
+        for i in 1:currdim
+            sigvec[:,k] = sigvec[:,k] + alpha[i,k] * bvec[:,i]
+        end
+       
+    end
+
+    #
+    # Collapse the subspace to be spanned by the lowest-lying Ritz vectors
+    #
+
+    # New subspace dimension
+    cache.currdim = cache.blocksize
+    cache.nnew = cache.blocksize
+
+    # Save the Ritz vectors as the new subspace vectors
+    for k in 1:blocksize
+       bvec[:,k] = sigvec[:,k]
+    end
+
 end
